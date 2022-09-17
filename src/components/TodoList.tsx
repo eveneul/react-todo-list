9f577164223155a00df7c4928fd173bf3b4b06b0
오하늘 <nebulass@naver.com>
2022-09-17 23:13:00 +0900

    selector를 이용해 category filter화

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,41 +1,34 @@
+import React from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
-import { toDoSelector, toDoState } from '../atoms';
+import { categoryState, toDoSelector, toDoState } from '../atoms';
 import CreateTodo from './CreateTodo';
 import Todo from './Todo';
 
 function TodoList() {
-	const [todo, doing, done] = useRecoilValue(toDoSelector);
+	const toDos = useRecoilValue(toDoSelector);
+	const [category, setCategory] = useRecoilState(categoryState);
+	const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+		const {
+			currentTarget: { value },
+		} = event;
+		setCategory(value);
+		// 선택한 option에 있는 데이터만 호출되게
+	};
 	return (
 		<>
 			<h1>To Do list</h1>
 			<hr />
 
 			<div className='input-wrapper'>
+				<select value={category} onInput={onInput}>
+					<option value='TO_DO'>To Do</option>
+					<option value='DOING'>Doing</option>
+					<option value='DONE'>Done</option>
+				</select>
 				<CreateTodo />
-				<section>
-					<h2>To-Do</h2>
-					<ul>
-						{todo.map((data) => (
-							<Todo key={data.id} {...data} />
-						))}
-					</ul>
-				</section>
-				<section>
-					<h2>Doing</h2>
-					<ul>
-						{doing.map((data) => (
-							<Todo key={data.id} {...data} />
-						))}
-					</ul>
-				</section>
-				<section>
-					<h2>Done</h2>
-					<ul>
-						{done.map((data) => (
-							<Todo key={data.id} {...data} />
-						))}
-					</ul>
-				</section>
+				{toDos?.map((data) => (
+					<Todo key={data.id} {...data} />
+				))}
 			</div>
 		</>
 	);
