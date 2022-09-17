3d44abf1a6381b0e87a77f1dc10ac688a970c9b8
오하늘 <nebulass@naver.com>
2022-09-17 14:06:15 +0900

    react-hook-form, useForm을 이용해 간편하게 입력값 알아보기

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,34 +1,59 @@
 import React, { FormEvent, useState } from 'react';
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
 
 function Todo() {
-	const [toDo, setToDo] = useState('');
-	const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-		const {
-			currentTarget: { value },
-		} = e;
-		setToDo(value);
-	};
+	const { register, watch } = useForm();
 
-	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		console.log(toDo);
-	};
+	// register: onBlur, onChange, onClick, ref... 를 return 해 주는 함수
+	// watch: 유저가 input에 입력한 내용을 바로바로 return 해 줌
+	console.log(watch());
 
 	return (
-		<div className='todo-wrap'>
-			<form onSubmit={onSubmit}>
-				<legend>Todo list</legend>
-				<input
-					onChange={onChange}
-					value={toDo}
-					type='text'
-					placeholder='오늘의 할 일을 적어 주세요'
-				/>
-				<button>Add</button>
-			</form>
-		</div>
+		<>
+			<div className='input-todo'>
+				<form action=''>
+					<legend>Todolist</legend>
+					<input
+						{...register('toDo')}
+						type='text'
+						placeholder='오늘의 할일을 적어주세요'
+					/>
+					<button>Add</button>
+				</form>
+			</div>
+		</>
 	);
 }
+
+// function Todo() {
+// 	const [toDo, setToDo] = useState('');
+// 	const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+// 		const {
+// 			currentTarget: { value },
+// 		} = e;
+// 		setToDo(value);
+// 	};
+
+// 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+// 		e.preventDefault();
+// 		console.log(toDo);
+// 	};
+
+// 	return (
+// 		<div className='todo-wrap'>
+// 			<form onSubmit={onSubmit}>
+// 				<legend>Todo list</legend>
+// 				<input
+// 					onChange={onChange}
+// 					value={toDo}
+// 					type='text'
+// 					placeholder='오늘의 할 일을 적어 주세요'
+// 				/>
+// 				<button>Add</button>
+// 			</form>
+// 		</div>
+// 	);
+// }
 
 export default Todo;
