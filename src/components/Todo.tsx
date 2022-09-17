49203d3f50268790a9cf1de3126b668ce9ac7b5e
오하늘 <nebulass@naver.com>
2022-09-17 21:25:09 +0900

    onClick 이벤트로 무슨 버튼을 눌렀는지 파악

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import { IToDo } from '../atoms';
+import { useSetRecoilState } from 'recoil';
+import { IToDo, toDoState } from '../atoms';
 
-function Todo({ text }: IToDo) {
-	return <li>{text}</li>;
+function Todo({ text, category, id }: IToDo) {
+	const setTodos = useSetRecoilState(toDoState);
+	const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+		const {
+			currentTarget: { name },
+		} = event;
+		console.log(name);
+	};
+	return (
+		<li>
+			<span>{text}</span>
+			{category !== 'DOING' && (
+				<button name='DOING' onClick={onClick}>
+					Doing
+				</button>
+			)}
+			{category !== 'TO_DO' && (
+				<button name='TO_DO' onClick={onClick}>
+					To do
+				</button>
+			)}
+			{category !== 'DONE' && (
+				<button name='DONE' onClick={onClick}>
+					Done
+				</button>
+			)}
+		</li>
+	);
 }
 
 export default Todo;
