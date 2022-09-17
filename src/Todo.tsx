7dbaccd37b3a0a7105605f4dc1fbfdc68832bca7
오하늘 <nebulass@naver.com>
2022-09-17 16:06:13 +0900

    react hook form을 이용한 custom validation

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,8 +1,10 @@
 import { useForm } from 'react-hook-form';
 
-interface IErrors {
+interface IForm {
 	email: string;
 	msg: string;
+	password: string;
+	rePassword: string;
 }
 
 function Todo() {
@@ -10,10 +12,18 @@ function Todo() {
 		register,
 		handleSubmit,
 		formState: { errors },
-	} = useForm<IErrors>();
-	const onValid = (data: any) => {};
+		setError, //setError: 특정한 상황에서 오류를 발생시킴
+	} = useForm<IForm>();
+	const onValid = (data: IForm) => {
+		if (data.password !== data.rePassword) {
+			setError(
+				'rePassword',
+				{ message: '입력한 비번이 동일하지 않아요' },
+				{ shouldFocus: true }
+			);
+		}
+	};
 	console.log(errors);
-
 	return (
 		<>
 			<div className='input-todo'>
@@ -35,47 +45,39 @@ function Todo() {
 						placeholder='메시지 입력하세요'
 						{...register('msg', {
 							required: '메시지 입력해',
-							minLength: {
-								value: 5,
-								message: '5글자 이상 입력해',
+							validate: {
+								noDog: (value) =>
+									value.includes('dog')
+										? 'dog이 포함된 아이디는 사용할 수 없습니다'
+										: true,
+								noCat: (value) =>
+									value.includes('cat')
+										? 'cat이 포함된 아이디는 사용할 수 없습니다'
+										: true,
 							},
+							// validate는 boolean을 return
+							// dog이 포함된 글자는 허용 X
+							// 예를 들어 이미 가입된 아이디를 걸러낼 때 사용
 						})}
 					/>
+					<span>{errors?.msg?.message}</span>
+					<input
+						type='password'
+						placeholder='비밀번호1'
+						{...register('password', { required: '비번 입력하세요' })}
+					/>
+					<span>{errors?.password?.message}</span>
+					<input
+						type='password'
+						placeholder='비밀번호 재입력하세요'
+						{...register('rePassword', { required: '비번 입력하세요' })}
+					/>
+					<span>{errors.rePassword?.message}</span>
 					<button>Add</button>
 				</form>
 			</div>
 		</>
 	);
 }
-
-// function Todo() {
-// 	const [toDo, setToDo] = useState('');
-// 	const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-// 		const {
-// 			currentTarget: { value },
-// 		} = e;
-// 		setToDo(value);
-// 	};
-
-// 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-// 		e.preventDefault();
-// 		console.log(toDo);
-// 	};
-
-// 	return (
-// 		<div className='todo-wrap'>
-// 			<form onSubmit={onSubmit}>
-// 				<legend>Todo list</legend>
-// 				<input
-// 					onChange={onChange}
-// 					value={toDo}
-// 					type='text'
-// 					placeholder='오늘의 할 일을 적어 주세요'
-// 				/>
-// 				<button>Add</button>
-// 			</form>
-// 		</div>
-// 	);
-// }
 
 export default Todo;
