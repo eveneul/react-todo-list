f36e03e7a0cf5947d8563725a06339e6ead49ecd
오하늘 <nebulass@naver.com>
2022-09-17 15:13:53 +0900

    상황별로 다르게 오류 메시지 출력

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,18 +1,9 @@
-import React, { FormEvent, useState } from 'react';
-import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 
 function Todo() {
-	const { register, watch, handleSubmit } = useForm();
-	//handleSubmit: form validation 담당
-	//handleSubmit(데이터가 유효할때, 데이터가 유효하지 않을때)
-	//handleSubmit의 onInvalid는 필수적이지 않지만 onValid는 필수적
-
-	//hook-form에서 required를 적으면 누군가 개발자도구를 열어서 html을 수정할 수도 없고, 사용자가 입력을 안 했을 시 자동으로 입력을 안 한 곳으로 focus, 마우스가 이동해서 사용자의 편의성을 도와준다
-
-	const onValid = (data: any) => {
-		console.log(data);
-	};
+	const { register, handleSubmit, formState } = useForm();
+	const onValid = (data: any) => {};
+	console.log(formState.errors);
 
 	return (
 		<>
@@ -20,9 +11,19 @@ function Todo() {
 				<form onSubmit={handleSubmit(onValid)}>
 					<legend>Todolist</legend>
 					<input
-						{...(register('toDo'), { required: true, minLength: 10 })}
+						placeholder='user email'
+						{...register('email', { required: '이메일입력해요' })}
+					/>
+					<input
 						type='text'
-						placeholder='오늘의 할일을 적어주세요'
+						placeholder='메시지 입력하세요'
+						{...register('msg', {
+							required: '메시지 입력해',
+							minLength: {
+								value: 5,
+								message: '5글자 이상 입력해',
+							},
+						})}
 					/>
 					<button>Add</button>
 				</form>
